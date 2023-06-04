c8a748f23f950d30372108cd01e42e22aee0a9d7
SavaadMP <savaadmoorkkanad@gmail.com>
2023-06-04 17:16:58 +0530

    login and register page designed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 // * Components
 import Header from "./components/Header/Header";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 };
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-white p-10">
+    <header className="bg-white fixed w-full p-10 flex items-center justify-between shadow-lg">
       <div className="brandLogo">
-        <h1 className="font-bold text-2xl">BookStore 📚</h1>
+        <h1 className="font-bold text-xl">BookStore 📚</h1>
       </div>
+
+      <nav>
+        <ul className="flex text-lg font-bold">
+          <li className="mr-5">
+            <Link to="/login">Login</Link>
+          </li>
+
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
